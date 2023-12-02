75eb91796b986aea12f4b198e39423500b5471eb
Michal-Devops <michailllada@gmail.com>
2023-12-02 23:12:04 +0100

    dalsze zmiany na mysql

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# vagrant-zabbix
-creating zabbix serverw on vagrant 
+create ubuntu20 machine which will be the main machine for zabbix server , install postqress database , ngnix intslation , create bash script 
+
+at first boot , i forgot to install postqress so it was giving errors 
+
+
+when installing zabbix with postgres database, I experienced a problem with an unsupported version of the database 
+
+I will now try to use mysql 
--- ubuntu-zabbix-server/zabbix-mysql.sh
+++ ubuntu-zabbix-server/zabbix-mysql.sh
@@ -4,16 +4,18 @@
 sudo apt update
 sudo apt upgrade -y
 
-# Instalacja PostgreSQL
-sudo apt install -y postgresql postgresql-contrib
+# Instalacja MySQL
+sudo apt install -y mysql-server mysql-client
 
-# Uruchomienie i włączenie PostgreSQL
-sudo systemctl start postgresql
-sudo systemctl enable postgresql
+# Uruchomienie i włączenie MySQL
+sudo systemctl start mysql
+sudo systemctl enable mysql
 
 # Tworzenie użytkownika i bazy danych dla Zabbix
-sudo -u postgres psql -c "CREATE USER zabbix WITH PASSWORD 'twoje_hasło';"
-sudo -u postgres psql -c "CREATE DATABASE zabbix OWNER zabbix;"
+sudo mysql -e "CREATE USER 'zabbix'@'localhost' IDENTIFIED BY 'admin';"
+sudo mysql -e "CREATE DATABASE zabbix CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;"
+sudo mysql -e "GRANT ALL PRIVILEGES ON zabbix.* TO 'zabbix'@'localhost';"
+sudo mysql -e "FLUSH PRIVILEGES;"
 
 # Instalacja repozytorium Zabbix
 wget https://repo.zabbix.com/zabbix/6.4/ubuntu/pool/main/z/zabbix-release/zabbix-release_6.4-1+ubuntu20.04_all.deb
@@ -21,16 +23,19 @@ dpkg -i zabbix-release_6.4-1+ubuntu20.04_all.deb
 sudo apt update
 
 # Instalacja serwera Zabbix, frontendu, agenta
-sudo apt install -y zabbix-server-pgsql zabbix-frontend-php php7.4-pgsql zabbix-nginx-conf zabbix-sql-scripts zabbix-agent
+sudo apt install -y zabbix-server-mysql zabbix-frontend-php zabbix-nginx-conf zabbix-sql-scripts zabbix-agent
 
 # Import schematu i danych początkowych do bazy danych Zabbix
-#sudo zcat /usr/share/doc/zabbix-sql-scripts/postgresql/server.sql.gz | sudo -u zabbix psql zabbix bledna sciezk 
-sudo  zcat /usr/share/zabbix-sql-scripts/postgresql/server.sql.gz | sudo -u zabbix psql zabbix
+#zcat /usr/share/doc/zabbix-sql-scripts/mysql/create.sql.gz | mysql -u zabbix -p zabbix
+sudo zcat /usr/share/zabbix-sql-scripts/mysql/server.sql.gz | mysql --default-character-set=utf8mb4 -u zabbix -p admin
 # Konfiguracja Zabbix server
 sudo sed -i 's/# DBPassword=/DBPassword=twoje_hasło/' /etc/zabbix/zabbix_server.conf
 sudo sed -i "s/# DBHost=localhost/DBHost=localhost/" /etc/zabbix/zabbix_server.conf
+sudo sed -i "s/# DBName=zabbix/DBName=zabbix/" /etc/zabbix/zabbix_server.conf
+sudo sed -i "s/# DBUser=zabbix/DBUser=zabbix/" /etc/zabbix/zabbix_server.conf
 
 # Konfiguracja PHP dla frontendu Zabbix
+sudo sed -i 's/# php_value date.timezone Europe/Riga/php_value date.timezone Europe/Warsaw/' /etc/zabbix/nginx.conf
 sudo sed -i 's/# listen 80;/listen 8080;/' /etc/zabbix/nginx.conf
 sudo sed -i 's/# server_name example.com;/server_name zabbixvm;/' /etc/zabbix/nginx.conf
 
@@ -41,3 +46,4 @@ sudo nginx -t && sudo systemctl restart nginx
 # Uruchomienie serwera Zabbix i agenta
 sudo systemctl restart zabbix-server zabbix-agent php7.4-fpm
 sudo systemctl enable zabbix-server zabbix-agent nginx php7.4-fpm
+
